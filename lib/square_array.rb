8547b23d5a7558c79708911d792a208daf8e9f6e
Chaya Deaver <chayadeaver@gmail.com>
2019-08-01 20:04:36 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,7 @@
 require 'pry'
-numbers = [1,2,3]
- def square_array(numbers)
-   i = 0 
+# numbers = [1,2,3] 
+def square_array(numbers)
+  i = 0 
   while i < numbers.length do 
     binding.pry
     numbers[i]
